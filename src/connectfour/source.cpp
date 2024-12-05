092bf67de81d21be0866eae0d8aceb8c47f5b329
evunei <80120337+evunei@users.noreply.github.com>
2024-12-04 23:03:13 -0500

    fixed diagonals, changed a few things.

--- src/connectfour/source.cpp
+++ src/connectfour/source.cpp
@@ -15,9 +15,6 @@ public:
              << endl;
         cout << "Let's Play Connect Four! Coded by Nico." << endl;
 
-        
-        size[0] = sizeof(board) / sizeof(board[0]);
-        size[1] = sizeof(board[0]) / sizeof(board[0][0]);
         cout << "Enter Player 1's Name: ";
         cin >> players[0];
         cout << "Enter Player 2's Name: ";
@@ -26,9 +23,9 @@ public:
 
         cout << "Initializing Game..." << endl;
 
-        for (int i = 0; i < size[0]; i++)
+        for (int i = 0; i < 6; i++)
         {
-            for (int j = 0; j < size[1]; j++)
+            for (int j = 0; j < 7; j++)
             {
                 board[i][j] = defaultString;
             };
@@ -45,122 +42,107 @@ public:
             /* code */
             doTurn();
         }
+
+        // New Game
+        cout << "Play Again? (y/n): ";
+        char choice;
+        cin >> choice;
+        cout << endl;
+        if (choice == 'y' || choice == 'Y')
+        {
+            Game newgame;
+        }
     }
 
-    bool checkWin()
+    const bool checkWin() // Made Constant
     {
-
+        //cout << "Debug: Entering checkWin" << endl;
         /*
         Winning Cases:
         - Horizontal 4
         - Vertical 4
         - Diagonal 4
         */
-        // Horizontal Check (Checked, Works.)
-        for (int i = 0; i < size[0]; i++)
-        {
-            for (int j = 0; j < size[1]-4; j++)
-            {
-               if( ( board[i][j] == board[i][j+1] ) && ( board[i][j] == board[i][j+2] ) && ( board[i][j] == board[i][j+3] ) && (board[i][j]!=defaultString) ){
-                    return true;
-                }
-            };
-        };
-
-        // Vertical Check (Checked, Works.)
-        for (int i = 0; i < size[0]; i++)
-        {
-            for (int j = 0; j < size[1]-4; j++)
-            {
-                if( ( board[i][j] == board[i+1][j] ) && ( board[i][j] == board[i+2][j] ) && ( board[i][j] == board[i+3][j] ) && (board[i][j]!=defaultString) ){
-                    return true;
-                }
-            };
-        };
-
-        // Diagonal Check 1 
-        for (int i = 0; i < size[0]-4; i++)
-        {
-            for (int j = 0; j < size[1]-4; j++)
-            {
-                if( 
-                    board[i][j]!=defaultString 
-                    &&
-                    board[i][j] == board[i+1][j+1]
-                    &&
-                    board[i][j] == board[i+2][j+2]
-                    &&
-                    board[i][j] == board[i+3][j+3]
-                ){
-                    return true;
-                }
-            };
-        };
-
-        // Diagonal Check 2
-        for (int i = size[0]; i > 4 ; --i)
-        {
-            for (int j = size[1]; j > 4 ; --j)
-            {
-                if( 
-                    board[i][j]!=defaultString 
-                    &&
-                    board[i][j] == board[i-1][j-1]
-                    &&
-                    board[i][j] == board[i-2][j-2]
-                    &&
-                    board[i][j] == board[i-3][j-3]
-                ){
-                    return true;
-                }
-            };
-        };
-
-        // Diagonal Check 3
-        for (int i = 0; i < size[0]-4; i++)
-        {
-            for (int j = size[1]; j > 4 ; --j)
-            {
-                if( 
-                    board[i][j]!=defaultString 
-                    &&
-                    board[i][j] == board[i+1][j-1]
-                    &&
-                    board[i][j] == board[i+2][j-2]
-                    &&
-                    board[i][j] == board[i+3][j-3]
-                ){
-                    return true;
-                }
-            };
-        };
-
-        // Diagonal Check 4
-        for (int i = size[0]; i > 4 ; --i)
-        {
-            for (int j = 0; j < size[1]-4; j++)
-            {
-                if( 
-                    board[i][j]!=defaultString 
-                    &&
-                    board[i][j] == board[i-1][j+1]
-                    &&
-                    board[i][j] == board[i-2][j+2]
-                    &&
-                    board[i][j] == board[i-3][j+3]
-                ){
-                    return true;
-                }
-            };
-        };
-
         
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                // Horizontal Check (Checked, Works.)
+                
+                if (j < 4){
+                    if ((board[i][j] == board[i][j + 1]) && (board[i][j] == board[i][j + 2]) && (board[i][j] == board[i][j + 3]) && (board[i][j] != defaultString))
+                    {
+                        return true;
+                    }
+                }
 
+                 // Vertical Check (Checked, Works.)
+                if(i < 3){
+                    if ((board[i][j] == board[i + 1][j]) && (board[i][j] == board[i + 2][j]) && (board[i][j] == board[i + 3][j]) && (board[i][j] != defaultString))
+                    {
+                        return true;
+                    }
+                }
+
+                // Diagonal Check 1 
+                if( i<3 && j>4 ){
+                    if (
+                        board[i][j] != defaultString &&
+                        board[i][j] == board[i + 1][j + 1] &&
+                        board[i][j] == board[i + 2][j + 2] &&
+                        board[i][j] == board[i + 3][j + 3])
+                    {
+                        return true;
+                    }
+                }
+                
+                // Diagonal Check 2 
+                if( i<3 && j<4 ){
+                    if (
+                        board[i][j] != defaultString &&
+                        board[i][j] == board[i + 1][j - 1] &&
+                        board[i][j] == board[i + 2][j - 2] &&
+                        board[i][j] == board[i + 3][j - 3])
+                    {
+                        return true;
+                    }
+                }
+
+                // Diagonal Check 3
+                if( i>=3 && j>4 ){
+                    if (
+                        board[i][j] != defaultString &&
+                        board[i][j] == board[i - 1][j + 1] &&
+                        board[i][j] == board[i - 2][j + 2] &&
+                        board[i][j] == board[i - 3][j + 3])
+                    {
+                        return true;
+                    }
+                }
+
+                // Diagonal Check 4 
+                if( i<3 && j>=3 ){
+                    if (
+                        board[i][j] != defaultString &&
+                        board[i][j] == board[i + 1][j - 1] &&
+                        board[i][j] == board[i + 2][j - 2] &&
+                        board[i][j] == board[i + 3][j - 3])
+                    {
+                        return true;
+                    }
+                }
+
+            };
+        };
+
+        //cout << "Debug: Exiting doTurn" << endl;
         return false;
     }
 
-    void doTurn()
+    void doTurn() 
     {
+        //cout << "Debug: Entering doTurn" << endl;
         currentPlayer = currentPlayer == 1 ? 2 : 1;
         int x, y = -1;
         cout << "Player " << currentPlayer << " (" << players[currentPlayer - 1] << "), Where would you like to put your chip (X)?" << endl;
@@ -169,7 +151,7 @@ public:
         cout << endl;
 
         // find available Y in the board.
-        for (int i = size[0] - 1; i > -1; i--)
+        for (int i = 5; i > -1; i--)
         {
             if (board[i][x - 1] == defaultString)
             {
@@ -179,38 +161,66 @@ public:
         };
 
         // check if x,y is valid
-        if ((0 >= x || x > size[1]) || (0 >= y || y > size[0]) || (board[y - 1][x - 1] != defaultString))
-        {
-            cout << "Invalid Coordinate Passed. Try Again.";
-        }
-        else
-        {
-            board[y - 1][x - 1] = currentPlayer == 1 ? "X" : "O";
 
-            printBoard();
-            if(checkWin()){
-                canPlay = false;
+        // Neatened Code
+        if (cin.fail() || x < 1 || x > 7)
+        {
+            cout << "Invalid input, Moving On." << endl;
+            cin.clear();
+            cin.ignore(INT_MAX, '\n');
 
-                cout << "Player " << currentPlayer << " (" << players[currentPlayer - 1] << "), Has Won!" << endl;
-            }
+            return;
         }
+
+        board[y - 1][x - 1] = currentPlayer == 1 ? "X" : "O";
+
+        printBoard();
+        if (checkWin())
+        {
+            canPlay = false;
+
+            cout << "Player " << currentPlayer << " (" << players[currentPlayer - 1] << "), Has Won!" << endl;
+            return;
+        }
+
+        if (y == -1) {
+            cout << "Column full! Choose another column." << endl;
+
+            int left = 0;
+            // Check if All Columns are filled
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (board[i][j] == defaultString)
+                    {
+                        left++;
+                    }
+                };
+            };
+            //cout << "Debug: " << left << endl;
+            canPlay = left > 0;
+
+            return;
+        }
+        //cout << "Debug: Exiting doTurn" << endl;
+        
     }
 
     // Getter
-    void printBoard()
+    const void printBoard() // Made Constant
     {
-        for (int i = 0; i < size[0]; i++)
+        for (int i = 0; i < 6; i++)
         {
-            cout << i + 1 << "   ";
-            for (int j = 0; j < size[1]; j++)
+            cout << i + 1 << " ";
+            for (int j = 0; j < 7; j++)
             {
                 cout << board[i][j] << " ";
             };
             cout << "\n";
         };
-        cout << "\n";
-        cout << "    ";
-        for (int i = 0; i < size[1]; i++)
+        cout << "  ";
+        for (int i = 0; i < 7; i++)
         {
             cout << i + 1 << " ";
         }
@@ -220,17 +230,14 @@ public:
 private:
     string board[6][7];
     int currentPlayer;
-    int size[2];
-    string defaultString = "-";
+    const string defaultString = "-"; // Made Constant
     bool canPlay = true;
 };
 
 int main()
 {
-    Game *game = new Game();
+    Game game;
     // game.printBoard();
     cout << sizeof(game);
-    delete game;
-
     return 0;
 }
