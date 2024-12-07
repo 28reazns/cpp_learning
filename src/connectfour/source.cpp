6a351e3578ec7d50c8db1e5c2c2374bd67db3b7f
evunei <80120337+evunei@users.noreply.github.com>
2024-12-07 18:43:42 -0500

    fixed other bugs

--- src/connectfour/source.cpp
+++ src/connectfour/source.cpp
@@ -7,12 +7,7 @@ public:
     string players[2];
     Game()
     {
-        cout << "\n"
-             << "\n"
-             << "\n"
-             << "\n"
-             << "\n"
-             << endl;
+
         cout << "Let's Play Connect Four! Coded by Nico." << endl;
 
         cout << "Enter Player 1's Name: ";
@@ -23,11 +18,11 @@ public:
 
         cout << "Initializing Game..." << endl;
 
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < ROWS; i++)
         {
-            for (int j = 0; j < 7; j++)
+            for (int j = 0; j < COLS; j++)
             {
-                board[i][j] = defaultString;
+                board[i][j] = TOKEN;
             };
         };
 
@@ -56,39 +51,43 @@ public:
 
     const bool checkWin() // Made Constant
     {
-        //cout << "Debug: Entering checkWin" << endl;
+        // cout << "Debug: Entering checkWin" << endl;
         /*
         Winning Cases:
         - Horizontal 4
         - Vertical 4
         - Diagonal 4
         */
-        
-        for (int i = 0; i < 6; i++)
+
+        for (int i = 0; i < ROWS; i++)
         {
-            for (int j = 0; j < 7; j++)
+            for (int j = 0; j < COLS; j++)
             {
                 // Horizontal Check (Checked, Works.)
-                
-                if (j < 4){
-                    if ((board[i][j] == board[i][j + 1]) && (board[i][j] == board[i][j + 2]) && (board[i][j] == board[i][j + 3]) && (board[i][j] != defaultString))
+
+                /*
+                if (j < 4)
+                {
+                    if ((board[i][j] == board[i][j + 1]) && (board[i][j] == board[i][j + 2]) && (board[i][j] == board[i][j + 3]) && (board[i][j] != TOKEN))
                     {
                         return true;
                     }
                 }
 
-                 // Vertical Check (Checked, Works.)
-                if(i < 3){
-                    if ((board[i][j] == board[i + 1][j]) && (board[i][j] == board[i + 2][j]) && (board[i][j] == board[i + 3][j]) && (board[i][j] != defaultString))
+                // Vertical Check (Checked, Works.)
+                if (i < 3)
+                {
+                    if ((board[i][j] == board[i + 1][j]) && (board[i][j] == board[i + 2][j]) && (board[i][j] == board[i + 3][j]) && (board[i][j] != TOKEN))
                     {
                         return true;
                     }
                 }
 
-                // Diagonal Check 1 
-                if( i<3 && j>4 ){
+                // Diagonal Check 1
+                if (i < 3 && j > 4)
+                {
                     if (
-                        board[i][j] != defaultString &&
+                        board[i][j] != TOKEN &&
                         board[i][j] == board[i + 1][j + 1] &&
                         board[i][j] == board[i + 2][j + 2] &&
                         board[i][j] == board[i + 3][j + 3])
@@ -96,11 +95,12 @@ public:
                         return true;
                     }
                 }
-                
-                // Diagonal Check 2 
-                if( i<3 && j<4 ){
+
+                // Diagonal Check 2
+                if (i < 3 && j < 4)
+                {
                     if (
-                        board[i][j] != defaultString &&
+                        board[i][j] != TOKEN &&
                         board[i][j] == board[i + 1][j - 1] &&
                         board[i][j] == board[i + 2][j - 2] &&
                         board[i][j] == board[i + 3][j - 3])
@@ -110,9 +110,10 @@ public:
                 }
 
                 // Diagonal Check 3
-                if( i>=3 && j>4 ){
+                if (i >= 3 && j > 4)
+                {
                     if (
-                        board[i][j] != defaultString &&
+                        board[i][j] != TOKEN &&
                         board[i][j] == board[i - 1][j + 1] &&
                         board[i][j] == board[i - 2][j + 2] &&
                         board[i][j] == board[i - 3][j + 3])
@@ -121,10 +122,11 @@ public:
                     }
                 }
 
-                // Diagonal Check 4 
-                if( i<3 && j>=3 ){
+                // Diagonal Check 4
+                if (i < 3 && j >= 3)
+                {
                     if (
-                        board[i][j] != defaultString &&
+                        board[i][j] != TOKEN &&
                         board[i][j] == board[i + 1][j - 1] &&
                         board[i][j] == board[i + 2][j - 2] &&
                         board[i][j] == board[i + 3][j - 3])
@@ -132,18 +134,21 @@ public:
                         return true;
                     }
                 }
-
+                */
+               if ( fourInARow(i,j,1,1) || fourInARow(i,j,1,-1) || fourInARow(i,j,-1,1) || fourInARow(i,j,-1,-1) || fourInARow(i,j,1,0) || fourInARow(i,j,0,1) ){
+                    return true;
+               }
             };
         };
 
-        //cout << "Debug: Exiting doTurn" << endl;
+        // cout << "Debug: Exiting doTurn" << endl;
         return false;
     }
 
-    void doTurn() 
+    void doTurn()
     {
-        //cout << "Debug: Entering doTurn" << endl;
-        currentPlayer = currentPlayer == 1 ? 2 : 1;
+        // cout << "Debug: Entering doTurn" << endl;
+        currentPlayer = (currentPlayer%2)+1; // modulo
         int x, y = -1;
         cout << "Player " << currentPlayer << " (" << players[currentPlayer - 1] << "), Where would you like to put your chip (X)?" << endl;
         cout << "X: ";
@@ -153,7 +158,7 @@ public:
         // find available Y in the board.
         for (int i = 5; i > -1; i--)
         {
-            if (board[i][x - 1] == defaultString)
+            if (board[i][x - 1] == TOKEN)
             {
                 y = i + 1;
                 break;
@@ -163,7 +168,7 @@ public:
         // check if x,y is valid
 
         // Neatened Code
-        if (cin.fail() || x < 1 || x > 7)
+        if (cin.fail() || x < 1 || x > COLS)
         {
             cout << "Invalid input, Moving On." << endl;
             cin.clear();
@@ -183,44 +188,49 @@ public:
             return;
         }
 
-        if (y == -1) {
-            cout << "Column full! Choose another column." << endl;
+        if (y == -1)
+        {
+            
 
             int left = 0;
             // Check if All Columns are filled
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < ROWS; i++)
             {
-                for (int j = 0; j < 7; j++)
+                for (int j = 0; j < COLS; j++)
                 {
-                    if (board[i][j] == defaultString)
+                    if (board[i][j] == TOKEN)
                     {
                         left++;
                     }
                 };
             };
-            //cout << "Debug: " << left << endl;
+            // cout << "Debug: " << left << endl;
             canPlay = left > 0;
+            if (canPlay){
+                cout << "Column full! Choose another column." << endl;
+            }else{
+                cout << "Board full! Nobody Wins." << endl;
+            }
 
             return;
         }
-        //cout << "Debug: Exiting doTurn" << endl;
-        
+        // cout << "Debug: Exiting doTurn" << endl;
     }
 
     // Getter
     const void printBoard() // Made Constant
     {
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < ROWS; i++)
         {
             cout << i + 1 << " ";
-            for (int j = 0; j < 7; j++)
+            for (int j = 0; j < COLS; j++)
             {
                 cout << board[i][j] << " ";
             };
             cout << "\n";
         };
         cout << "  ";
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < COLS; i++)
         {
             cout << i + 1 << " ";
         }
@@ -228,10 +238,41 @@ public:
     }
 
 private:
-    string board[6][7];
+    const static int ROWS = 6;
+    const static int COLS = 7; // both constant variables now
+    string board[ROWS][COLS];
     int currentPlayer;
-    const string defaultString = "-"; // Made Constant
+    const string TOKEN = "-"; // Made Constant
     bool canPlay = true;
+
+    // helper functions
+    bool fourInARow(int x, int y, int dx, int dy){
+        string curAt = board[x][y]; // currently at, AKA the current token.
+        if( curAt == TOKEN ){
+            //cerr << "Default Token" << endl;
+            return false;
+        }
+        
+        //cerr << "x: " << x+1 << " y: " << y+1 << " dx: " << dx << " dy: " << dy << endl;
+
+        // assure that x + (4 * dx) and y + (4 * dy) are in bounds
+
+        if ( !( (-1 < (x + (4 * dx)) && (x + (4 * dx)) < ROWS) && (-1 < (y + (4 * dy)) && (y + (4 * dy)) < COLS)  ) ){
+            //cerr << "(" <<(x + (4 * dx))+1 << "," << (y + (4 * dy))+1 << ") Out of Bounds" << endl;
+            return false;
+        }
+
+        //cerr << "(" <<(x + (4 * dx))+1 << "," << (y + (4 * dy))+1 << ") In Bounds" << endl;
+        
+        for ( int i = 0; i < 4; i++){
+            
+            if(board[x + i * dx][y + i * dy] != curAt){
+                //cerr << "Doesnt Equal" << board[x + 1 * dx][y + 1 * dy] << endl;
+                return false;
+            }
+        }
+        return true;
+    }
 };
 
 int main()
